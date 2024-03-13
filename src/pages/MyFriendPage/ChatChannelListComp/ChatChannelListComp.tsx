1e8285727f53c814b1d2fe62c956c021365d85b3
ksohyeon0302 <ksohyeon0302@gmail.com>
2024-03-13 23:47:33 +0900

    ref: 채팅 내역 저장, 불러오기

--- src/pages/MyFriendPage/ChatChannelListComp/ChatChannelListComp.tsx
+++ src/pages/MyFriendPage/ChatChannelListComp/ChatChannelListComp.tsx
@@ -1,23 +1,57 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 import styles from "./ChatChannelListComp.module.css";
 import { call } from "service/ApiService";
 import { Channel } from "../MyFriendPage";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { AuthState } from "../ChatSpaceComp/ChatSpaceComp";
 
 interface OwnProps {
   channelList: Channel[];
   setChannelList: React.Dispatch<React.SetStateAction<Channel[]>>;
+  setCurrentChannel: React.Dispatch<React.SetStateAction<Channel>>;
 }
 
-const ChatChannelListComp = ({ channelList, setChannelList }: OwnProps) => {
+const ChatChannelListComp = ({
+  channelList,
+  setChannelList,
+  setCurrentChannel,
+}: OwnProps) => {
+  const myNickname = useSelector((state: AuthState) => state.auth.nickname);
   const navigate = useNavigate();
-  const handleOpenChannel = useCallback((channelId: number) => {
-    navigate("/friends?channel_id=" + channelId);
+  const handleOpenChannel = useCallback((channel: Channel) => {
+    setCurrentChannel(channel);
+    navigate("/friends?channel_id=" + channel.channelId);
   }, []);
+
+  const channelNameList = useMemo(() => {
+    if (channelList.length === 0) return [];
+    const list = channelList.map((channel) => {
+      if (channel.members.length === 2) {
+        if (channel.members[0].nickname === myNickname)
+          return channel.members[1].nickname;
+        return channel.members[0].nickname;
+      } else if (channel.members.length > 2) {
+        if (channel.members[0].nickname === myNickname)
+          return (
+            channel.members[1].nickname +
+            " 외" +
+            (channel.members.length - 1) +
+            "명"
+          );
+        return (
+          channel.members[0].nickname +
+          " 외" +
+          (channel.members.length - 2) +
+          "명"
+        );
+      }
+    });
+    return list;
+  }, [channelList]);
 
   useEffect(() => {
     call("/channel/all", "GET").then((response) => {
-      console.log(response);
       setChannelList(response);
     });
   }, []);
@@ -46,11 +80,11 @@ const ChatChannelListComp = ({ channelList, setChannelList }: OwnProps) => {
                 });
               }}
             >
-              {channel.channelName}
+              {channelNameList[idx]}
               {channel.choosen && (
                 <button
                   className={styles["chat-btn"]}
-                  onClick={() => handleOpenChannel(channel.channelId)}
+                  onClick={() => handleOpenChannel(channel)}
                 >
                   채팅하기
                 </button>
